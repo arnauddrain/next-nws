f586f160feb5fbfca09959806765c6486f54ddb2
Arnaud Drain <drain.arnaud@hotmail.fr>
2025-03-31 13:38:20 +0200

    fix types

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -2,7 +2,7 @@ import { Compteur } from "../../components/compteur";
 
 export default async function Games() {
     const data = await fetch('https://api.sampleapis.com/switch/games');
-    const games = await data.json();
+    const games: { id: string, name: string }[] = await data.json();
 
     return (<div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <h1 className="text-3xl">Jeux</h1>
